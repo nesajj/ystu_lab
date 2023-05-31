fb22c8863612afaf171105387367ce4c1511c98c
nesajj <ilya_kiselev_01@list.ru>
2023-05-31 17:19:05 +0300

    new update - added exp

--- calculator/calculator/Program.cs
+++ calculator/calculator/Program.cs
@@ -11,14 +11,22 @@
             Console.WriteLine("Второе число:");
             b = Convert.ToDouble(Console.ReadLine());
 
-            Console.WriteLine("1.Сложение\n2.Вычетание\n3.Умножение\n4.Деление");
+            Console.WriteLine("1.Сложение\n2.Вычетание\n3.Умножение\n4.Деление\n5.Возвести в степень");
             op = Convert.ToInt32(Console.ReadLine());
             switch (op)
             {
                 case 1: result= a + b; break;
                 case 2: result= a - b; break;
                 case 3: result= a * b; break;
-                case 4: result = a / b; break;
+                case 4:
+                    if (b == 0)
+                    {
+                        Console.WriteLine("Деление на 0 невозможно!");
+                        return;
+                    }
+
+                    result = a / b; break;
+                case 5: result = Math.Pow(a, b); break;
             }
             Console.Write("Ответ: " + result); 
         }
