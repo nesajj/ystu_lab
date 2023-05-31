593ef0cbf6667dfe9e17e2ccf957a93a2306e093
nesajj <ilya_kiselev_01@list.ru>
2023-05-31 16:20:23 +0300

    new files: .gitignore && C# project

---
+++ calculator/calculator/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace calculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double a, b, result = 0;
+            int op;
+            Console.WriteLine("Первое число:");
+            a = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Второе число:");
+            b = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("1.Сложение\n2.Вычетание\n3.Умножение\n4.Деление");
+            op = Convert.ToInt32(Console.ReadLine());
+            switch (op)
+            {
+                case 1: result= a + b; break;
+                case 2: result= a - b; break;
+                case 3: result= a * b; break;
+                case 4: result = a / b; break;
+            }
+            Console.Write("Ответ: " + result); 
+        }
+    }
+}
